110002b96371b33ea25c0dd925e0e1b3ecca9bf6
Ak <akshithgray17@gmail.com>
2024-10-17 18:44:21 +0530

    modified header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import './globals.css';
 
+export const metadata = {
+  title: 'Anime',
+  description: 'Watch anime',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -1,17 +1,25 @@
 import Link from 'next/link'
 import React from 'react'
+import Search from './Search'
+import { Button } from './ui/button'
 
 const NavigationBar = () => {
   return (
-    <div className='flex'>
-        <header>
-            <Link href='/' className="cursor-pointer text-3xl">
+
+    <header className='flex justify-between gap-4 min-w-full'>
+       <div className='flex gap-4'>
+            <Link href='/' className="cursor-pointer text-3xl text-center">
                 KITSUNE
             </Link>
+            <Search/>
+       </div>
+        <div className='flex justify-end'>
+            <Button className="bg-zinc-800 rounded-xl">
+                Login
+            </Button>
+        </div>
+    </header>
 
-            <search/>
-      </header>
-    </div>
   )
 }
 
